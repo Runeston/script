1d2d1199855c157cb1453657f999ff8eb54bcf3f
Runeston <123926670+Runeston@users.noreply.github.com>
2023-01-31 12:19:34 +0800

    Add coolapk.js

---
+++ coolapk.js
@@ -0,0 +1,71 @@
+// 2023-01-01 14:03
+
+var url = $request.url;
+var body = $response.body;
+
+function adAppName(adUrls) {
+  if (/^https:\/\/api.coolapk.com\/v6\/feed\/detail/.test(adUrls)) return "酷安-detail";
+  if (/^https:\/\/api.coolapk.com\/v6\/feed\/replyList/.test(adUrls)) return "酷安-replyList";
+  if (/^https:\/\/api.coolapk.com\/v6\/main\/dataList/.test(adUrls)) return "酷安-dataList";
+  if (/^https:\/\/api.coolapk.com\/v6\/main\/indexV8/.test(adUrls)) return "酷安-index";
+  return "";
+}
+
+if (!body) $done({});
+switch (adAppName(url)) {
+  case "酷安-replyList":
+    try {
+      let obj = JSON.parse(body);
+      obj.data = Object.values(obj.data).filter((item) => item.id);
+      body = JSON.stringify(obj);
+    } catch (error) {
+      console.log(`酷安-replyList, 出现异常`);
+    }
+    break;
+  case "酷安-detail":
+    try {
+      let obj = JSON.parse(body);
+      obj.data.hotReplyRows = Object.values(obj.data.hotReplyRows).filter(
+        (item) => item["id"]
+      );
+      obj.data.include_goods_ids = [];
+      obj.data.include_goods = [];
+      body = JSON.stringify(obj);
+    } catch (error) {
+      console.log(`酷安-detail, 出现异常`);
+    }
+    break;
+  case "酷安-dataList":
+    try {
+      let obj = JSON.parse(body);
+      obj.data = Object.values(obj.data).filter((item) =>
+        !(item["entityTemplate"] === "sponsorCard" || item.title === "精选配件")
+      );
+      body = JSON.stringify(obj);
+    } catch (error) {
+      console.log(`酷安-dataList, 出现异常`);
+    }
+    break;
+  case "酷安-index":
+    try {
+      let obj = JSON.parse(body);
+      obj.data = Object.values(obj.data).filter((item) =>
+        !(
+          item["entityTemplate"] === "sponsorCard" ||
+          item.entityId === 8639 ||
+          item.entityId === 33066 ||
+          item.entityId === 32557 ||
+          item.title.indexOf("值得买") !== -1 ||
+          item.title.indexOf("红包") !== -1
+        )
+      );
+      body = JSON.stringify(obj);
+    } catch (error) {
+      console.log(`酷安-index, 出现异常`);
+    }
+    break;
+  default:
+    break;
+}
+
+$done({ body });
