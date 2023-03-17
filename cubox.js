c67fc363c60eeb6e8eb037ec45b4e92bef08c587
Runeston <123926670+Runeston@users.noreply.github.com>
2023-03-17 23:01:07 +0800

    Add

--- cubox.js
+++ cubox.js
@@ -1,15 +1,3 @@
-*******************************
-
-[rewrite_local]
-
-^https:\/\/cubox\.(pro|cc)\/c\/api\/user(Info|Pay) url script-response-body https://raw.githubusercontent.com/Runeston/script/main/cubox.js
-
-[mitm] 
-
-hostname = cubox.cc
-
-*******************************/
-
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
