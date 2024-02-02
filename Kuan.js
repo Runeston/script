9d4a60f1bc8438c18629ad51dd24bb9201b7229e
Runeston <123926670+Runeston@users.noreply.github.com>
2024-02-02 14:37:12 +0800

    同步酷安js

--- Kuan.js
+++ Kuan.js
@@ -1,60 +1,80 @@
-// 2023-02-09 15:35
+// 2023-11-15 17:20
 
-if (!$response.body) $done({});
 const url = $request.url;
+if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (obj.data) {
-  // detail
-  if (url.includes("/feed/detail")) {
-    if (obj.data.hotReplyRows && obj.data.hotReplyRows.length > 0) {
-      obj.data.hotReplyRows = obj.data.hotReplyRows.filter((item) => item.id);
+if (url.includes("/feed/detail")) {
+  if (obj.data?.hotReplyRows?.length > 0) {
+    obj.data.hotReplyRows = obj.data.hotReplyRows.filter((item) => item?.id);
+  }
+  if (obj.data?.topReplyRows?.length > 0) {
+    obj.data.topReplyRows = obj.data.topReplyRows.filter((item) => item?.id);
+  }
+  const item = ["detailSponsorCard", "include_goods", "include_goods_ids"];
+  for (let i of item) {
+    if (obj.data?.[i]) {
+      obj.data[i] = [];
     }
-    if (obj.data.topReplyRows && obj.data.topReplyRows.length > 0) {
-      obj.data.topReplyRows = obj.data.topReplyRows.filter((item) => item.id);
-    }
-    if (obj.data.include_goods_ids) {
-      obj.data.include_goods_ids = [];
-    }
-    if (obj.data.include_goods) {
-      obj.data.include_goods = [];
-    }
-    if (obj.data.detailSponsorCard) {
-      obj.data.detailSponsorCard = [];
-    }
-  } else if (url.includes("/feed/replyList")) {
-    // replyList
-    if (obj.data.length > 0) {
-      obj.data = obj.data.filter((item) => item.id);
-    }
-  } else if (url.includes("/main/dataList")) {
-    // dataList
-    obj.data = obj.data.filter(
-      (item) =>
-        !(item.entityTemplate === "sponsorCard" || item.title === "精选配件")
-    );
-  } else if (url.includes("/main/indexV8")) {
-    // index
+  }
+} else if (url.includes("/feed/replyList")) {
+  if (obj.data?.length > 0) {
+    obj.data = obj.data.filter((item) => item?.id);
+  }
+} else if (url.includes("/main/dataList")) {
+  if (obj.data?.length > 0) {
+    obj.data = obj.data.filter((item) => !(item?.entityTemplate === "sponsorCard" || item?.title === "精选配件"));
+  }
+} else if (url.includes("/main/indexV8")) {
+  if (obj.data?.length > 0) {
     obj.data = obj.data.filter(
       (item) =>
         !(
-          item.entityTemplate === "sponsorCard" ||
-          item.entityId === 8639 ||
-          item.entityId === 29349 ||
-          item.entityId === 33006 ||
-          item.entityId === 32557 ||
-          item.title.includes("值得买") ||
-          item.title.includes("红包")
+          item?.entityTemplate === "sponsorCard" ||
+          item?.entityId === 8639 ||
+          item?.entityId === 29349 ||
+          item?.entityId === 33006 ||
+          item?.entityId === 32557 ||
+          item?.title?.includes("值得买") ||
+          item?.title?.includes("红包")
         )
     );
-  } else if (url.includes("/page/dataList")) {
-    // 酷安热搜
+  }
+} else if (url.includes("/main/init")) {
+  // 整体配置
+  if (obj.data?.length > 0) {
+    let newDatas = [];
+    for (let item of obj.data) {
+      // 944热门搜索 945开屏广告 6390首页Tab
+      if ([944, 945, 6390]?.includes(item?.entityId)) {
+        continue;
+      } else {
+        if (item?.entityId === 20131) {
+          // 发现页 顶部项目
+          if (item?.entities?.length > 0) {
+            let newEnts = [];
+            for (let i of item.entities) {
+              if (i?.title === "酷品") {
+                continue;
+              } else {
+                newEnts.push(i);
+              }
+            }
+            item.entities = newEnts;
+          }
+        }
+        newDatas.push(item);
+      }
+    }
+    obj.data = newDatas;
+  }
+} else if (url.includes("/page/dataList")) {
+  if (obj.data?.length > 0) {
     obj.data = obj.data.filter(
       (item) =>
-        !(item.title === "酷安热搜")
+        !(item?.title === "酷安热搜" || item?.entityTemplate === "imageScaleCard" || item?.entityTemplate === "sponsorCard")
     );
   }
 }
 
-body = JSON.stringify(obj);
-$done({ body });
+$done({ body: JSON.stringify(obj) });
