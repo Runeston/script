103f98138d0b0c612150e286221973a60c1f4d33
Runeston <123926670+Runeston@users.noreply.github.com>
2025-02-07 20:15:08 +0800

    Add

--- gog.js
+++ gog.js
@@ -1,8 +1,8 @@
 /*************************************
 
 [rewrite_local]
-^https:\/\/isi\.*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Runeston/script/refs/heads/main/gg.js
-^https:\/\/isi\.*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Runeston/script/refs/heads/main/gg.js
+^https:\/\/isi\.*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Runeston/script/refs/heads/main/gog.js
+^https:\/\/isi\.*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Runeston/script/refs/heads/main/gog.js
 
 [mitm]
 hostname = isi.csan.*.com
