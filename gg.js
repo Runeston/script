53348c31a5b2a12fc2e384e15708939b3d7dd719
Runeston <123926670+Runeston@users.noreply.github.com>
2025-02-07 20:10:42 +0800

    Add

--- gg.js
+++ gg.js
@@ -1,8 +1,8 @@
 /*************************************
 
 [rewrite_local]
-^https:\/\/isi\.*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/gg.js
-^https:\/\/isi\.*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/gg.js
+^https:\/\/isi\.*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Runeston/script/refs/heads/main/gg.js
+^https:\/\/isi\.*\.g.*\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Runeston/script/refs/heads/main/gg.js
 
 [mitm]
 hostname = isi.csan.*.com
