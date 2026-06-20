55ff41dc7cb078db14b8776f0fdc795f37e50fd1
Runeston <123926670+Runeston@users.noreply.github.com>
2026-06-20 10:36:14 +0800

    mambo_rcoin_83.js

---
+++ mambo_rcoin_83.js
@@ -0,0 +1,22 @@
+let body = $response.body || '';
+let headers = $response.headers || {};
+
+try {
+  const data = JSON.parse(body);
+
+  if (data && data.user && Object.prototype.hasOwnProperty.call(data.user, 'rCoin')) {
+    data.user.rCoin = 83;
+  }
+
+  body = JSON.stringify(data);
+
+  for (const key of Object.keys(headers)) {
+    const k = key.toLowerCase();
+    if (k === 'content-length' || k === 'etag') delete headers[key];
+  }
+
+  $done({ body, headers });
+} catch (e) {
+  body = body.replace(/("rCoin"\s*:\s*)0\.83\b/, (_, p1) => p1 + '83');
+  $done({ body, headers });
+}
