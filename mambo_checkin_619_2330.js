24ab7ce1663a15c942e2da66d3788552f7147346
Runeston <123926670+Runeston@users.noreply.github.com>
2026-06-20 10:46:53 +0800

    mambo_checkin_619_2330.js

--- mambo_checkin_619_2330.js
+++ mambo_checkin_619_2330.js
@@ -1,17 +1,11 @@
 let body = $response.body || '';
 let headers = $response.headers || {};
 
-const targetTime = "2026-06-19T15:30:00.000Z"; // 北京时间 2026-06-19 23:30:00
-
 try {
   const data = JSON.parse(body);
 
-  if (Object.prototype.hasOwnProperty.call(data, "checkInTime")) {
-    data.checkInTime = targetTime;
-  }
-
   if (Object.prototype.hasOwnProperty.call(data, "hasCheckedInToday")) {
-    data.hasCheckedInToday = true;
+    data.hasCheckedInToday = false;
   }
 
   body = JSON.stringify(data);
@@ -24,8 +18,8 @@ try {
   $done({ body, headers });
 } catch (e) {
   body = body.replace(
-    /("checkInTime"\s*:\s*")[^"]+(")/,
-    `$1${targetTime}$2`
+    /("hasCheckedInToday"\s*:\s*)true\b/,
+    "$1false"
   );
   $done({ body, headers });
 }
